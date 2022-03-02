3b6ffd38c80cb976a448a48f30a710dc54722569
D1183-Mustafa <93746275+D1183-Mustafa@users.noreply.github.com>
2022-03-02 20:22:14 +0300

    challange-4 eklendi

---
+++ 4-challange.js
@@ -0,0 +1,21 @@
+// In this Kata the aim is to compare each pair of integers from 2 arrays, and return a new array of large numbers.
+
+// Note: Both arrays have the same dimensions.
+
+// Example:
+
+// let arr1 = [13, 64, 15, 17, 88];
+// let arr2 = [23, 14, 53, 17, 80];
+// getLargerNumbers(arr1, arr2); // Returns [23, 64, 53, 17, 88]
+
+
+function getLargerNumbers(a, b) {
+    let c = [];
+    for (let i = 0; i < a.length; i++) {
+        c.push(Math.max(a[i],b[i]))
+        
+    }
+    return c
+}
+
+console.log(getLargerNumbers([13, 64, 15, 17, 88],[23, 14, 53, 17, 80] ));
